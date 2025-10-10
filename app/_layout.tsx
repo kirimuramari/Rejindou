e77cea381262c85ad59324cec60e7bc9e6376615
kirimuramari <lifemastert@gmail.com>
2025-10-10 19:28:01 +0900

    ヘッダー非表示

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
-
+import { View } from "react-native";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View style={{ flex: 1, backgroundColor: "#f0f0f0" }}>
+      <Stack options={{ headerShown: false }} />
+    </View>
+  );
 }
